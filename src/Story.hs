6c361303838c930cad7a3b4e5aefcf002781709c
Franck Verrot <franck@verrot.fr>
2018-08-19 17:11:19 -0700

    Create a story type data type `Type`

--- src/Story.hs
+++ src/Story.hs
@@ -6,8 +6,9 @@ import qualified GHC.Generics as Generics
 import qualified Text.Printf as Printf
 import qualified Data.Text as Text
 
+{- Story -}
 data Story = Story { kind :: String
-                   , story_type :: String
+                   , story_type :: Type
                    , estimate :: Maybe Int
                    , current_state :: State
                    , name :: String
@@ -17,13 +18,32 @@ instance Show Story where
   show story =
     Printf.printf "[%-8s] %-64s (%s)" storyType storyName storyEstimate
     where
-      storyType = story_type story
-      storyName = name story
+      storyType     = show . story_type $ story
+      storyName     = name story
       storyEstimate = (Maybe.fromMaybe "-" (fmap show $ estimate story))
 
 instance JSON.FromJSON Story
 
 
+{- Type -}
+data Type = Feature
+          | Chore
+          | Bug
+          | Release
+          deriving (Generics.Generic, Show)
+
+instance JSON.FromJSON Type where
+  parseJSON = JSON.withText "Type"
+                $ \type' ->
+                    case type' of
+                      "feature" -> return Feature
+                      "chore"   -> return Chore
+                      "bug"     -> return Bug
+                      "release" -> return Release
+                      unknown -> fail $ "type " ++ Text.unpack unknown ++ "is unknown"
+
+
+{- State -}
 data State = Unstarted
            | Started
            | Delivered
